07e6cdfad15e78af0bdea99c8f1be56ff11d4872
baraaAlosaily <seelbaraa@gmail.com>
2021-04-12 20:12:07 +0300

    201 class 02 task

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ alert('welcome Greeting Mr./Ms. ' + whatName);
 function answer1() {
   let spOrt = prompt('Did you like sport?').toLowerCase();
   if (spOrt=== 'yes') {
-    alert('Thats Great keep going');
+    alert('Yes, so Thats Great keep going');
   } else {
-    alert('You have to choose at lease one kind of sport to practice every day');
+    alert('No, so You have to choose at lease one kind of sport to practice every day');
   }
   //   console.log(spOrt);
 
@@ -21,9 +21,9 @@ answer1();
 function answer2() {
   let manSaf = prompt('Did you like Mansaf?').toLowerCase();
   if (manSaf === 'yes') {
-    alert('You are original Jordanian');
+    alert('Yes, so You are original Jordanian');
   } else {
-    alert('you have to try it is so delicios');
+    alert('No, so you have to try it is so delicios');
   }
   //   console.log(manSaf);
 }
@@ -32,9 +32,9 @@ answer2();
 function answer3() {
   let coDing = prompt('Did you like coding?').toLowerCase();
   if (coDing === 'yes') {
-    alert('Great and i do');
+    alert('Yes, so Great and i do');
   } else {
-    alert('You have to to Ask about ASAC collage they teach coding very well');
+    alert('No, so You have to to Ask about ASAC collage they teach coding very well');
   }
   //   console.log(coDing);
 }
@@ -43,9 +43,9 @@ answer3();
 function answer4() {
   let reaDing = prompt('Did you like reading books?').toLowerCase();
   if (reaDing === 'yes') {
-    alert('Thats Great keep going');
+    alert('Yes, so Thats Great keep going');
   } else {
-    alert('You have to read because readers are a leader');
+    alert('No, so You have to read because readers are a leader');
   }
   //   console.log(reaDing);
 }
@@ -54,9 +54,9 @@ answer4();
 function answer5() {
   let coFfee = prompt('Did you like drink coffee?').toLowerCase();
   if (coFfee === 'yes') {
-    alert('I think i is not good but i do to wakeup');
+    alert('Yes, so I think i is not good but i do to wakeup');
   } else {
-    alert('Good, i feeling jealous from you');
+    alert('No, so Good, i feeling jealous from you');
   }
   //   console.log(coFfee);
 }
