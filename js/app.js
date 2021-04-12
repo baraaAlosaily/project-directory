81bdcf2c9c7eaa53bfde0ff58c2fbef71dd8c142
baraaAlosaily <seelbaraa@gmail.com>
2021-04-12 20:42:43 +0300

    201 class 02 task

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 'use strict';
 
 alert('Welcome to My Wesite');
+// console.log('Welcome to My Wesite');
 
 let whatName = prompt('Let us know you name');
 alert('welcome Greeting Mr./Ms. ' + whatName);
+// console.log('welcome Greeting Mr./Ms. ' + whatName);
 
 
 function answer1() {
   let spOrt = prompt('Did you like sport?').toLowerCase();
   if (spOrt=== 'yes') {
     alert('Yes, so Thats Great keep going');
+    // console.log('Yes, so Thats Great keep going');
   } else {
     alert('No, so You have to choose at lease one kind of sport to practice every day');
+    // console.log('No, so You have to choose at lease one kind of sport to practice every day');
   }
-  //   console.log(spOrt);
-
 }
 answer1();
 
@@ -22,10 +24,11 @@ function answer2() {
   let manSaf = prompt('Did you like Mansaf?').toLowerCase();
   if (manSaf === 'yes') {
     alert('Yes, so You are original Jordanian');
+    // console.log('Yes, so You are original Jordanian');
   } else {
     alert('No, so you have to try it is so delicios');
+    // console.log('No, so you have to try it is so delicios');
   }
-  //   console.log(manSaf);
 }
 answer2();
 
@@ -33,10 +36,11 @@ function answer3() {
   let coDing = prompt('Did you like coding?').toLowerCase();
   if (coDing === 'yes') {
     alert('Yes, so Great and i do');
+    // console.log('Yes, so Great and i do');
   } else {
     alert('No, so You have to to Ask about ASAC collage they teach coding very well');
+    // console.log('No, so You have to to Ask about ASAC collage they teach coding very well');
   }
-  //   console.log(coDing);
 }
 answer3();
 
@@ -44,10 +48,11 @@ function answer4() {
   let reaDing = prompt('Did you like reading books?').toLowerCase();
   if (reaDing === 'yes') {
     alert('Yes, so Thats Great keep going');
+    // console.log('Yes, so Thats Great keep going');
   } else {
     alert('No, so You have to read because readers are a leader');
+    // console.log('No, so You have to read because readers are a leader');
   }
-  //   console.log(reaDing);
 }
 answer4();
 
@@ -55,10 +60,11 @@ function answer5() {
   let coFfee = prompt('Did you like drink coffee?').toLowerCase();
   if (coFfee === 'yes') {
     alert('Yes, so I think i is not good but i do to wakeup');
+    // console.log('Yes, so I think i is not good but i do to wakeup');
   } else {
     alert('No, so Good, i feeling jealous from you');
+    // console.log('No, so Good, i feeling jealous from you');
   }
-  //   console.log(coFfee);
 }
 answer5();
 
